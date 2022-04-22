006fa8e6ee8ec086676d207369daafc9e1150a93
mattix7771 <mattione7@gmail.com>
2022-04-22 17:32:02 +0100

    added move class

---
+++ Move.cs
@@ -0,0 +1,12 @@
+class Move 
+{
+    private int val {set; get;}
+    private int x {set; get;}
+    private int y {set; get;}
+
+  Move(int val, int x, int y){
+      this.val = val;
+      this.x = x;
+      this.y = y;
+  }
+}
